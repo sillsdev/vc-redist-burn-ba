c2b2f3db64c5bd75657657928bc405b86b9996e6
tombogle <tom_bogle@sil.org>
2025-04-23 17:19:39 -0400

    WIP: Began to flesh out the details

---
+++ src/CustomBA/BootstrapperApplication.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using WixToolset.Mba.Core;
+
+namespace SIL.VCRedistBurnBA
+{
+    public class CustomBootstrapperApplication : BootstrapperApplication
+    {
+        private const int kDefaultMinMinorVersion = 38;
+        private int _minMinorVersion;
+
+        public CustomBootstrapperApplication(IEngine engine) : base(engine)
+        {
+        }
+
+        protected override void Run()
+        {
+            engine.Log(LogLevel.Standard, "Starting Custom Bootstrapper Application...");
+
+            try
+            {
+                _minMinorVersion = (int)engine.GetVariableNumeric("MinMinorVersion");
+            }
+            catch
+            {
+                engine.Log(LogLevel.Error, "Failed to retrieve MinMinorVersion parameter. Using default value.");
+                _minMinorVersion = kDefaultMinMinorVersion;
+            }
+
+            if (!VcRedistHelper.IsInstalled(_minMinorVersion))
+            {
+                engine.Log(LogLevel.Standard, $"VC++ Redistributable 14.{_minMinorVersion} or " +
+                                              "later not detected. Proceeding with installation.");
+
+                var pathToInstaller = VcRedistHelper.DownloadInstaller();
+                if (pathToInstaller == null)
+                {
+                    engine.Log(LogLevel.Error, "Failed to download VC++ Redistributable installer.");
+                    engine.SetVariableString("VcRedistInstallerPath", "", false);
+                    engine.Quit(1);
+                    return;
+                }
+                engine.SetVariableString("VcRedistInstallerPath", pathToInstaller, false);
+            }
+            else
+            {
+                engine.Log(LogLevel.Standard, "VC++ Redistributable already installed. " +
+                                              "Skipping installation.");
+                engine.SetVariableString("VcRedistInstallerPath", "", false);
+            }
+
+            engine.Quit(0);
+        }
+    }
+
+}
---
+++ src/CustomBA/VcRedistHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+
+namespace SIL.VCRedistBurnBA
+{
+    public static class VcRedistHelper
+    {
+        public static bool IsInstalled(int minMinorVersion = 1)
+        {
+            const string registryKey = @"SOFTWARE\Microsoft\VisualStudio\14.0\VC\Runtimes\x64";
+            using (var key = Registry.LocalMachine.OpenSubKey(registryKey))
+            {
+                if (key != null)
+                {
+                    var installed = key.GetValue("Installed");
+                    var minorVersion = key.GetValue("Minor");
+                    return installed != null && (int)installed == 1 && 
+                        minorVersion != null && (int)minorVersion >= minMinorVersion;
+                }
+            }
+            return false;
+        }
+
+        public static string DownloadInstaller()
+        {
+            // TODO: Implement the logic to download the VC++ Redistributable installer.
+            return "";
+        }
+    }
+}
